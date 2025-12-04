9cf117d7265d67e1dd9f6aab7cb18feba1a06029
riboust <159730263+ManuRib1@users.noreply.github.com>
2025-12-04 18:21:08 +0100

    v2 ma

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 
+let time = 1800; // 30 min
+
+function startTimer() {
+  setInterval(() => {
+    const min = Math.floor(time / 60).toString().padStart(2, "0");
+    const sec = (time % 60).toString().padStart(2, "0");
+
+    document.getElementById("timer").innerText = `⏳ ${min}:${sec}`;
+
+    if (time === 0) {
+      alert("💀 Temps écoulé ! Le directeur vous a attrapés !");
+      window.location.href = "index.html";
+    }
+
+    time--;
+  }, 1000);
+}
+
+window.onload = startTimer;
+
+
+
 function checkEnigma1() {
   const value = document.getElementById("e1").value.toLowerCase();
 
@@ -6,6 +28,8 @@ function checkEnigma1() {
     window.location.href = "enigma2.html";
   } else {
     alert("Mauvaise idée ! Le directeur ne bougera pas...");
+    wrongAnswer();
+
   }
 }
 
@@ -17,6 +41,8 @@ function checkEnigma2() {
     window.location.href = "enigma3.html";
   } else {
     alert("Ce n'est pas le bon mot de passe… déchiffre bien l'indice !");
+    wrongAnswer();
+
   }
 }
 
@@ -25,7 +51,36 @@ function checkEnigma3() {
 
   if (value === "1337") {
     window.location.href = "success.html";
-  } else {
+  } else {    
     alert("Code incorrect !");
+    wrongAnswer();
   }
 }
+
+
+
+function wrongAnswer() {
+  const audio = new Audio("https://www.myinstants.com/media/sounds/wrong-answer-sound-effect.mp3");
+  audio.play();
+
+  document.body.classList.add("shake");
+  setTimeout(() => document.body.classList.remove("shake"), 500);
+}
+
+
+function typeConsole(text, speed = 30){
+  let i = 0;
+  const box = document.getElementById("console");
+  const interval = setInterval(() => {
+    box.textContent += text[i];
+    i++;
+    if(i >= text.length) clearInterval(interval);
+  }, speed);
+}
+
+window.onload = () => {
+  startTimer();
+  if(document.getElementById("console")){
+     typeConsole("Connexion au système...\nBypass du pare-feu...\nAccès autorisé.\n");
+  }
+};
